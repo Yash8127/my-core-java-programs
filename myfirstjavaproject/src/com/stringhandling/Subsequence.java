226815cc298fa62a4cdf04a5244b97c45cbbde4c
Yash8127 <yaswanthgurindapalli@gmail.com>
2026-02-16 11:22:40 +0530

    "Slight change in logic for Subsequence"

--- myfirstjavaproject/src/com/stringhandling/Subsequence.java
+++ myfirstjavaproject/src/com/stringhandling/Subsequence.java
@@ -2,25 +2,29 @@ package com.stringhandling;
 
 public class Subsequence {
 
-	private static boolean isSub(String s1, String s2) {
-		boolean flag = false;
-		for (int i = 0; i < s1.length(); i++) {
-			char ch = s1.charAt(i);
-			flag = false;
-			for (int j = 0; j < s2.length(); j++) {
-				if (ch == s2.charAt(j)) {
-					flag = true;
-				}
-			}
+    private static boolean isSub(String s1, String s2) {
 
-		}
+        int i = 0; // pointer for s1
+        int j = 0; // pointer for s2
 
-		return flag;
-	}
+        while (i < s1.length() && j < s2.length()) {
 
-	public static void main(String[] args) {
-		System.out.println("Main method started");
-		System.out.println(isSub("yaswanth", "yaswanthvarma"));
-	}
+            if (s1.charAt(i) == s2.charAt(j)) {
+                i++;   // move only when matched
+            }
 
+            j++;       // always move in s2
+        }
+
+        return i == s1.length();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main method started");
+
+        System.out.println(isSub("abc", "abdghc")); // true
+        System.out.println(isSub("aec", "abcde"));   // false
+        System.out.println(isSub("cba", "abc"));     // false
+    }
 }
+
