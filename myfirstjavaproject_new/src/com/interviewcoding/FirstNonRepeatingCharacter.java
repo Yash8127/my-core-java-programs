0563e561bcc5f4a438d441cecb00496065170e8b
Yash8127 <yaswanthgurindapalli@gmail.com>
2026-06-24 09:45:17 +0530

    feat: find first non-repeating character using  LinkedHashMap

---
+++ myfirstjavaproject_new/src/com/interviewcoding/FirstNonRepeatingCharacter.java
@@ -0,0 +1,25 @@
+package com.interviewcoding;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FirstNonRepeatingCharacter {
+
+	public static void main(String[] args) {
+
+		String str = "java";
+
+		Map<Character, Integer> map = new LinkedHashMap<>();
+
+		for (char ch : str.toCharArray()) {
+			map.put(ch, map.getOrDefault(ch, 0) + 1);
+		}
+
+		for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+			if (entry.getValue() == 1) {
+				System.out.println(entry.getKey());
+				break;
+			}
+		}
+	}
+}
