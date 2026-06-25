d73247d799ebf20270ef29073e1ff0039f8b6126
Yash8127 <yaswanthgurindapalli@gmail.com>
2026-06-25 13:04:04 +0530

    feat: implement prime number check using iterative

---
+++ myfirstjavaproject_new/src/com/interviewcoding/PrimeNumber.java
@@ -0,0 +1,23 @@
+package com.interviewcoding;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+
+		int num = 29;
+		boolean isPrime = true;
+
+		if (num <= 1) {
+			isPrime = false;
+		} else {
+			for (int i = 2; i <= Math.sqrt(num); i++) {
+				if (num % i == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+		}
+
+		System.out.println(isPrime ? "Prime" : "Not Prime");
+	}
+}
