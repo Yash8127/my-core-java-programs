e987b150bd42808420592bb0371ffb21a7dd6221
Yash8127 <yaswanthgurindapalli@gmail.com>
2026-07-01 13:38:19 +0530

    feat: count vowels and consonants using loop

---
+++ myfirstjavaproject_new/src/com/interviewcoding/CountVowelsAndConsonants.java
@@ -0,0 +1,27 @@
+package com.interviewcoding;
+
+public class CountVowelsAndConsonants {
+
+	public static void main(String[] args) {
+
+		String str = "Java Programming".toLowerCase();
+
+		int vowels = 0;
+		int consonants = 0;
+
+		for (char ch : str.toCharArray()) {
+
+			if (Character.isLetter(ch)) {
+
+				if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+					vowels++;
+				} else {
+					consonants++;
+				}
+			}
+		}
+
+		System.out.println("Vowels      : " + vowels);
+		System.out.println("Consonants  : " + consonants);
+	}
+}
