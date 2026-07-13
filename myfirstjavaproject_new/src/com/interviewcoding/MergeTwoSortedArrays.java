4008d66c7de806b6e013512b660c07e1170fbe3f
Yash8127 <yaswanthgurindapalli@gmail.com>
2026-07-13 22:36:28 +0530

    feat: merge two sorted arrays using two-pointer

---
+++ myfirstjavaproject_new/src/com/interviewcoding/MergeTwoSortedArrays.java
@@ -0,0 +1,44 @@
+package com.interviewcoding;
+
+import java.util.Arrays;
+
+public class MergeTwoSortedArrays {
+
+	public static void main(String[] args) {
+
+		int[] arr1 = {1, 3, 5, 7};
+		int[] arr2 = {2, 4, 6, 8};
+
+		int[] merged = merge(arr1, arr2);
+
+		System.out.println(Arrays.toString(merged));
+	}
+
+	public static int[] merge(int[] arr1, int[] arr2) {
+
+		int[] result = new int[arr1.length + arr2.length];
+
+		int i = 0;
+		int j = 0;
+		int k = 0;
+
+		while (i < arr1.length && j < arr2.length) {
+
+			if (arr1[i] <= arr2[j]) {
+				result[k++] = arr1[i++];
+			} else {
+				result[k++] = arr2[j++];
+			}
+		}
+
+		while (i < arr1.length) {
+			result[k++] = arr1[i++];
+		}
+
+		while (j < arr2.length) {
+			result[k++] = arr2[j++];
+		}
+
+		return result;
+	}
+}
